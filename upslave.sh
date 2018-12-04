3c684f2c31c0420d7fbff5cee64b3e72c95a95b9
Your Name <you@example.com>
2018-12-04 09:14:19 -0500

    errrot upslave

--- upslave.sh
+++ upslave.sh
@@ -1,12 +1,12 @@
-if [ ! -f status.txt ]; then
-statusfile=`awk {print $1} status.txt` # from master mysql -u root -e 'SHOW MASTER STATUS;'
-binlog=`awk {print $1} status.txt`   #from master
+#if [ ! -f status.txt ]; then
+db-master-ip= 10.0.2.17
+statusfile=`awk {'print $1'} status.txt` # from master mysql -u root -e 'SHOW MASTER STATUS;'
+binlog=`awk {'print $1'} status.txt`   #from master
 mysqldump -u root demo <employees.sql
-mysql -u root -e 'CHANGE MASTER TO MASTER_HOST=""$statusfile"", MASTER_USER="slave_user", MASTER_PASSWORD="changeme", MASTER_LOG_FILE="mysql-bin.000003", MASTER_LOG_P$
+echo "$statusfile"
+mysql -u root -e 'CHANGE MASTER TO MASTER_HOST="'$db-master-ip'", MASTER_USER="slave_user", MASTER_PASSWORD="changeme", MASTER_LOG_FILE="'$statusfile'", MASTER_LOG_POS="'$binlog'";
 mysql -u root -e 'START SLAVE;'
-mysql -u root -e 'SHOW SLAVE STATUS\G'
-else
-echo "not found config master"
-fi
-
-
+#mysql -u root -e 'SHOW SLAVE STATUS\G'
+#else
+#echo "not found config master"
+#fi
