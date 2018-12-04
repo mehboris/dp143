a865091a91f67793d2036933d71dda63f7482c59
Your Name <you@example.com>
2018-12-04 07:31:01 -0500

    new status `date +%Y-%m-%d.%H:%M:%S`

---
+++ Puppetfile
@@ -0,0 +1,8 @@
+forge 'https://forge.puppet.com'
+
+# Modules discovered by generate-puppetfile
+mod 'puppetlabs/mysql',     '7.0.0'
+mod 'puppetlabs/stdlib',    '5.1.0'
+mod 'puppetlabs/translate', '1.2.0'
+mod 'puppetlabs/ntp',    '7.3.0'
+mod 'puppetlabs/firewall', '1.14.0'
---
+++ upmaster.sh
@@ -0,0 +1,12 @@
+mysql -u root -e 'FLUSH TABLES WITH READ LOCK;'
+mysql -u root -e 'SHOW MASTER STATUS;'|grep mysql>status`date +%Y-%m-%d.%H:%M:%S`.txt
+mysqldump -u root demo <employees.sql 
+mysql -u root -e 'UNLOCK TABLES;'
+ git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+git add .
+git commit -am'new status `date +%Y-%m-%d.%H:%M:%S`'
+git -u push origin master
+echo "aall done"
+
+
