7088ba57d007b92be85471add7c345b5d1115ad2
Your Name <you@example.com>
2018-12-07 05:47:26 -0500

    change db, relocation

--- upslave.sh
+++ upslave.sh
@@ -2,7 +2,7 @@
 dbmasterip= 10.0.2.17
 statusfile=`awk {'print $1'} status.txt` # from master mysql -u root -e 'SHOW MASTER STATUS;'
 binlog=`awk {'print $2'} status.txt`   #from master
-mysqldump -u root demo <employees.sql
+mysqldump -u root demo < dumped/demo.sql
 echo "$statusfile"
 echo `mysql -u root -e 'CHANGE MASTER TO MASTER_HOST="'$dbmasterip'", MASTER_USER="slave_user", MASTER_PASSWORD="changeme", MASTER_LOG_FILE="'$statusfile'", MASTER_LOG_POS='"$binlog"';'`
 mysql -u root -e 'START SLAVE;'
