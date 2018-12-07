30d95119d662790800c6228cd582fb22880b3201
Your Name <you@example.com>
2018-12-07 05:02:32 -0500

    new status `date +%Y-%m-%d.%H:%M:%S`

--- upmaster.sh
+++ upmaster.sh
@@ -1,6 +1,8 @@
+mysql -u root -e 'SLAVE STOP;'
 mysql -u root -e 'FLUSH TABLES WITH READ LOCK;'
 mysql -u root -e 'SHOW MASTER STATUS;'|grep mysql>status.txt
 mysqldump -u root demo <employees.sql 
+mysqldump -u root --opt demo > employees.sql
 mysql -u root -e 'UNLOCK TABLES;'
  git config --global user.email "you@example.com"
   git config --global user.name "Your Name"
