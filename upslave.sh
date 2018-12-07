bf8963cbf2e64123f50ab510a22277e5b7bafec2
Your Name <you@example.com>
2018-12-07 02:32:17 -0500

    fix error slave

--- upslave.sh
+++ upslave.sh
@@ -1,12 +1,12 @@
 #if [ ! -f status.txt ]; then
-db-master-ip= 10.0.2.17
+dbmasterip= 10.0.2.17
 statusfile=`awk {'print $1'} status.txt` # from master mysql -u root -e 'SHOW MASTER STATUS;'
-binlog=`awk {'print $1'} status.txt`   #from master
+binlog=`awk {'print $2'} status.txt`   #from master
 mysqldump -u root demo <employees.sql
 echo "$statusfile"
-mysql -u root -e 'CHANGE MASTER TO MASTER_HOST="'$db-master-ip'", MASTER_USER="slave_user", MASTER_PASSWORD="changeme", MASTER_LOG_FILE="'$statusfile'", MASTER_LOG_POS="'$binlog'";
+echo `mysql -u root -e 'CHANGE MASTER TO MASTER_HOST="'$dbmasterip'", MASTER_USER="slave_user", MASTER_PASSWORD="changeme", MASTER_LOG_FILE="'$statusfile'", MASTER_LOG_POS='"$binlog"';'`
 mysql -u root -e 'START SLAVE;'
-#mysql -u root -e 'SHOW SLAVE STATUS\G'
+mysql -u root -e 'SHOW SLAVE STATUS\G'
 #else
 #echo "not found config master"
 #fi
